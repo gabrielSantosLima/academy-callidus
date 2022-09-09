69fb690d9b3919a909f201a6490b19640f92e2bb
Gabriel Lima <lima.gabrieldsantos@gmail.com>
2022-09-09 15:39:23 -0400

    Adiciona aula 03 de Node.js

---
+++ NodeJS/aula-03/README.md
@@ -0,0 +1,5 @@
+# Aula 03
+
+## Diferença entre frameworks e bibliotecas?
+- Framework: Uma implementação incompleta (genérico).
+- Library: Código com a finalidade de ser reutilizado.
---
+++ NodeJS/aula-03/atividade-01/src/app.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import crypto from 'crypto'
+
+const app = express()
+const port = process.env.port || 3000
+let books = []
+
+app.use(express.json())
+
+app.get('/', (req, resp) => {
+    return resp.send('<html><body><h1>Curso de Node.js com Márcio</h1></body><html>')
+})
+
+app.get('/books', (req, resp) => {
+    return resp.status(200).json(books)
+})
+
+app.get('/books/:id', (req, resp) => {
+    const {id} = req.params
+    return resp.status(200).json(books.find(book => book.id === id))
+})
+
+app.post('/books', (req, resp) => {
+    const {title} = req.body
+    const id = crypto.randomUUID()
+    books.push({id, title})
+    return resp.status(200).json({id, title})
+})
+
+app.delete('/books/:id', (req, resp) => {
+    const {id} = req.params
+    books = books.filter(book => book.id !== id)
+    return resp.status(200).json({message: "success.", now: new Date()})
+})
+
+app.listen(port, () => {
+    console.log("Server started...")
+})
